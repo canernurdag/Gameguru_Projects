70177fe7a89d71e7a63fad79753bd32f97c6608f
Osman Caner Nurdag <osmancanernurdag@gmail.com>
2024-06-06 23:05:33 +0300

    +Create: Zenject => ProjectContext and it's template of ManagerInstaller&SignalInstaller

---
+++ Assets/Scripts/Runtime/Zenject/Installers/ProjectContext/ProjectContextManagerInstaller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class ProjectContextManagerInstaller : MonoInstaller
+{
+	public override void InstallBindings()
+	{
+	}
+}
