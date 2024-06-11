10689dfc104ef3fb5893e9d4147fa9cfd401c1cc
Osman Caner Nurdag <osmancanernurdag@gmail.com>
2024-06-11 23:17:44 +0300

    +Implement: Input manager

--- Assets/Scripts/Runtime/Managers/InputManager.cs
+++ Assets/Scripts/Runtime/Managers/InputManager.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class InputManager : MonoBehaviour
 {
+	private SignalBus _signalBus;
+
+	[Inject]
+	public void Construct(SignalBus signalBus)
+	{
+		_signalBus = signalBus;
+	}
+
 	private void Update()
 	{
 		if (Input.GetMouseButtonDown(0))
 		{
-
+			_signalBus.Fire(new SignalOnInputDown());
 		}
 	}
 }
