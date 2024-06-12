448c63e618eac14db6f2ed54f9c539e0b6ca78b6
Osman Caner Nurdag <osmancanernurdag@gmail.com>
2024-06-13 00:26:51 +0300

    +Refactor: Input Manager => Prevent input when game is finished

--- Assets/Scripts/Runtime/Managers/InputManager.cs
+++ Assets/Scripts/Runtime/Managers/InputManager.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Runtime.Managers._GameManager;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,19 @@ using Zenject;
 public class InputManager : MonoBehaviour
 {
 	private SignalBus _signalBus;
+	private GameManager _gameManager;
 
 	[Inject]
-	public void Construct(SignalBus signalBus)
+	public void Construct(SignalBus signalBus, GameManager gameManager)
 	{
+		_gameManager = gameManager;
 		_signalBus = signalBus;
 	}
 
 	private void Update()
 	{
+		if (_gameManager.CurrentGameStateType != GameStateType.GameStarted) return;
+
 		if (Input.GetMouseButtonDown(0))
 		{
 			_signalBus.Fire(new SignalOnInputDown());
